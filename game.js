284694fffc911273b94fc9ac42bb13fdb14ef5c4
Kevin Chang <kc34@rice.edu>
2016-01-03 17:36:24 -0800

    Now its own file!
    Removed Board and Blocks, but added partial falling as a fun effect.

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
+var TEMPORAL_ANTIALIASING = false; // Doesn't look so good. Don't use!
+
 var EMPTY_BLOCK_COLOR = "#bbbbbb"
 var BLOCK_COLORS = ["#bb0000", "#00bb00", "#bbbb00", "#bb00bb", "#00bbbb"];
 
-var FLOAT_PERIOD = 0.00 // In seconds, how long the block will remain unmoving when swapped into midair.
+var FLOAT_PERIOD = 0.4 // In seconds, how long the block will remain unmoving when swapped into midair.
 var CLEAR_PERIOD = 1 // How long blocks will remain after being cleared.
-var DROP_SPEED = 0.1 // After the float period, how long block will remain at each height before dropping one.
+var DROP_SPEED = 50 // Blocks per second.
 
 var BOARD_HEIGHT = 12 // In blocks
 var BOARD_LENGTH = 6 // In blocks
@@ -30,15 +32,23 @@ Game.prototype.draw = function(accumulator) {
 	// Draw the blocks.
 	for (var i = 0; i < BOARD_HEIGHT; i++) {
 		for (var j = 0; j < BOARD_LENGTH; j++) {
-			if (!this.board.blocks[i][j].empty()) {
-				var block = this.board.blocks[i][j].color;
-				if (this.board.blocks[i][j].get_state() == "CLEAR") {
+			if (!this.board.block[i][j].empty()) {
+				var block = this.board.block[i][j].color;
+				if (this.board.block[i][j].get_state() == Block.StateEnum.CLEAR) {
 					block = block.replace("b", "f").replace("0", "2");
+				}
+				var percent_fall = 0;
+				if (this.board.block[i][j].get_state() == Block.StateEnum.FALL) {
+					if (!TEMPORAL_ANTIALIASING) {
+						accumulator = 0;
+					}
+					percent_fall = 1 - ((this.board.block[i][j].state_timer - accumulator) / (1.0 / DROP_SPEED));
+					if (percent_fall > 1) { percent_fall = 1;}
 				}
 				ctx.fillStyle = block;
 				ctx.fillRect(
 					b_c.left + j * block_length,
-					bot - (i + 1) * block_height,
+					bot - (i + 1 - percent_fall) * block_height,
 					block_length, block_height);
 			}
 		}
@@ -102,231 +112,13 @@ Game.prototype.keydown_handler = function(key) {
 			this.board.swap();
 			break;
 		default:
-			this.board.rise();
+			this.board.raise();
 			break;
 	}
 }
 Game.prototype.update = function(time_step) {
 	this.board.update();
 }
-
-
-var Board = function() {
-	
-	this.height = BOARD_HEIGHT;
-	this.width = BOARD_LENGTH;
-	this.blocks = new Array(BOARD_HEIGHT);
-	for (var x = 0; x < BOARD_HEIGHT; x++) {
-		this.blocks[x] = new Array();
-		for (var y = 0; y < BOARD_LENGTH; y++) {
-			this.blocks[x].push(EMPTY_BLOCK);
-		}
-	}
-	
-	for (var x = 0; x < BOARD_HEIGHT / 2; x++) {
-		this.rise();
-	}
-	
-	this.cursor = { "x" : 2, "y" : 5 };
-	
-}
-Board.prototype.update = function() {
-	
-	this.checkClear();
-
-	this.fall();
-
-	for (var i = 0; i < BOARD_HEIGHT; i++) {
-		for (var j = 0; j < BOARD_LENGTH; j++) {
-			this.blocks[i][j].state_timer += time_step;
-			if (this.blocks[i][j].get_state() == "CLEAR" && this.blocks[i][j].state_timer >= CLEAR_PERIOD) {
-				this.blocks[i][j] = EMPTY_BLOCK;
-			} 
-		}
-	}
-	
-}
-Board.prototype.swap = function() {
-	if (this.blocks[this.cursor.y][this.cursor.x].get_state() != "CLEAR") {
-		if (this.blocks[this.cursor.y][this.cursor.x + 1].get_state() != "CLEAR") {
-			temp = this.blocks[this.cursor.y][this.cursor.x];
-			this.blocks[this.cursor.y][this.cursor.x] = this.blocks[this.cursor.y][this.cursor.x + 1];
-			this.blocks[this.cursor.y][this.cursor.x + 1] = temp;
-		}
-	}
-}
-Board.prototype.checkClear = function() {
-	// Check for horizontal clears.
-	var h = matrix_make(BOARD_HEIGHT, BOARD_LENGTH, 1);
-	for (var i = 0; i < BOARD_HEIGHT; i++) {
-		for (var j = 1; j < BOARD_LENGTH; j++) {
-			if (this.blocks[i][j].empty()) {
-				h[i][j] = 0;
-			} else if (this.blocks[i][j].matches(this.blocks[i][j - 1])) {
-				h[i][j] = h[i][j - 1] + 1;
-			} else {
-				h[i][j] = 1;
-			}
-		}
-	}
-	
-	// Check for vertical clears.
-	var v = matrix_make(BOARD_HEIGHT, BOARD_LENGTH, 1);
-	for (var i = 1; i < BOARD_HEIGHT; i++) {
-		for (var j = 0; j < BOARD_LENGTH; j++) {
-			if (this.blocks[i][j].empty()) {
-				v[i][j] = 0;
-			} else if (this.blocks[i][j].matches(this.blocks[i - 1][j])) {
-				v[i][j] = v[i - 1][j] + 1;
-			} else {
-				v[i][j] = 1;
-			}
-		}
-	}
-	
-	// Make horizontal clears.
-	for (var i = 0; i < BOARD_HEIGHT; i++) {
-		for (var j = BOARD_LENGTH - 1; j >= 0; j--) {
-			if (h[i][j] >= 3) {
-				var blocks_to_clear = h[i][j];
-				for (var delta = 0; delta < blocks_to_clear; delta++) {
-					this.blocks[i][j - delta].set_state("CLEAR");
-					h[i][j - delta] = 0;
-				}
-			}
-		}
-	}
-	
-	// Make horizontal clears.
-	// TODO: Adapt
-	for (var i = 0; i < BOARD_HEIGHT; i++) {
-		for (var j = BOARD_LENGTH - 1; j >= 0; j--) {
-			if (v[i][j] >= 3) {
-				var blocks_to_clear = v[i][j];
-				for (var delta = 0; delta < blocks_to_clear; delta++) {
-					this.blocks[i - delta][j].set_state("CLEAR");
-					v[i - delta][j] = 0;
-				}
-			}
-		}
-	}
-}
-Board.prototype.fall = function() {
-	// Step 1: Convert mid-air RESTs in airs to FLOATs.
-	// Step 2: Convert FLOATs after grace period to FALLs.
-	// Step 3: Drop FALLs or convert to RESTs.
-	for (var j = 0; j < BOARD_LENGTH; j++) {
-		if (this.blocks[0][j].get_state() != "CLEAR") {
-			this.blocks[0][j].set_state("REST");
-		}
-	}
-	for (var i = 1; i < BOARD_HEIGHT; i++) {
-		for (var j = 0; j < BOARD_LENGTH; j++) {
-			// STEP 1
-			switch (this.blocks[i][j].get_state()) {
-				
-				case "REST":
-					if (this.blocks[i - 1][j].empty()) {
-						console.log(i + " " + j);
-						for (var x = i; x < BOARD_HEIGHT; x++) {
-							// TODO: INVESTIGATE
-							this.blocks[x][j].set_state("FLOAT");
-						}
-					}
-					break;
-				case "FLOAT":
-					if (this.blocks[i][j].state_timer >= FLOAT_PERIOD) {
-						this.blocks[i][j].set_state("FALL");
-					}
-					break;
-				case "FALL":
-					if (this.blocks[i - 1][j].empty() || this.blocks[i - 1][j].get_state() == "FALL") {
-						// TODO: INVESTIGATE
-						if (this.blocks[i][j].state_timer >= DROP_SPEED) {
-							this.blocks[i][j].state_timer = 0;
-							this.blocks[i - 1][j] = this.blocks[i][j];
-							this.blocks[i][j] = EMPTY_BLOCK;
-						}
-					}
-					else
-					{
-						this.blocks[i][j].set_state("REST");
-					}
-					break;
-			}
-		}
-	}
-	
-}
-Board.prototype.rise = function() {
-	for (var j = 0; j < BOARD_LENGTH; j++) {
-		if (!this.blocks[BOARD_HEIGHT - 1][j].empty()) {
-			console.log("LOSE");
-		}
-	}
-	for (var i = BOARD_HEIGHT - 2; i >= 0; i--) {
-		for (var j = 0; j < BOARD_LENGTH; j++) {
-			this.blocks[i + 1][j] = this.blocks[i][j];
-			this.blocks[i][j] = EMPTY_BLOCK;
-		}
-	}
-	
-	for (var j = 0; j < BOARD_LENGTH; j++) {
-		var possible_colors = new Array();
-		for (color in BLOCK_COLORS) {
-			var color_ok = true;
-			// See if color would cause a clear.
-			if (this.blocks[2][j].color == this.blocks[1][j].color) {
-				if (BLOCK_COLORS[color] == this.blocks[2][j].color) {
-					color_ok = false;
-				}
-			}
-			if (j >= 2) {
-				if (this.blocks[0][j - 2].color == this.blocks[0][j - 1].color) {
-					if (BLOCK_COLORS[color] == this.blocks[0][j - 1].color) {
-						color_ok = false;
-					}
-				}
-			}
-			if (color_ok) {
-				possible_colors.push(BLOCK_COLORS[color]);
-			}
-		}
-		this.blocks[0][j] = new Block(possible_colors);
-	}
-}
-
-var Block = function(block_colors) {
-	this.color = block_colors[Math.floor(Math.random() * block_colors.length)];
-	this.bright_color = this.color.replace("b", "f");
-	this.state = "REST"; // STATES SO FAR: REST, FALL (FLOAT), CLEAR
-	this.state_timer = 0;
-}
-Block.prototype.empty = function() {
-	return false;
-}
-Block.prototype.matches = function(otherBlock) {
-	block_is_matchable = (this.state == "REST" || this.state == "FLOAT");
-	other_block_is_matchable = (otherBlock.state == "REST") || (this.state == "FLOAT");
-	colors_match = this.color == otherBlock.color;
-	return block_is_matchable && other_block_is_matchable && colors_match;
-}
-Block.prototype.get_state = function() {
-	return this.state;
-}
-Block.prototype.set_state = function(new_state) {
-	this.state = new_state;
-	this.state_timer = 0;
-}
-Block.random = function() {
-	return new Block(BLOCK_COLORS);
-}
-
-var EMPTY_BLOCK = new Block([EMPTY_BLOCK_COLOR]);
-EMPTY_BLOCK.matches = function(otherBlock) { return false; }
-EMPTY_BLOCK.empty = function() { return true; }
-EMPTY_BLOCK.set_state = function() { this.state = "EMPTY" } // TODO: INVESTIGATE
-EMPTY_BLOCK.set_state("EMPTY");
 
 var matrix_make = function(height, width, default_val) {
 	var my_matrix = new Array();
