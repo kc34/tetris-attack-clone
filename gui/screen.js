c40ae2fb20926402799fdf8008f8c477c924bd06
kevin <kc34@rice.edu>
2016-09-26 12:59:06 -0500

    Added documentation

--- gui/screen.js
+++ gui/screen.js
@@ -1,24 +1,27 @@
-// TODO: Learn to make things not fail silently
-
+/**
+ * Abstract Screen class.
+ *
+ * @author: artlessAvian
+ */
 var Screen = function() {
 
 	if (this.constructor === Screen) {
-		console.log("Cannot initialize abstract class!");
+		throw new error("Cannot initialize abstract class!");
 	}
 }
 
 Screen.prototype.update = function(dt) {
-	console.log("Abstract screen");
+	throw new error("Cannot initialize abstract class!");
 }
 
 Screen.prototype.draw = function(accumulator) {
-	console.log("Abstract screen");
+	throw new error("Cannot initialize abstract class!");
 }
 
 Screen.prototype.keydown_handler = function(key) {
-	console.log("Abstract screen");
+	throw new error("Cannot initialize abstract class!");
 }
 
 Screen.prototype.keyup_handler = function(key) {
-	console.log("Abstract screen");
+	throw new error("Cannot initialize abstract class!");
 }
